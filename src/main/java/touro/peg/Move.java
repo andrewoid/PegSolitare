7930dc3cd5b9208ff6c76c2d29942995d40a422c
SaraYaffe <syaffe@student.touro.edu>
2022-09-21 19:14:43 -0400

    added Move class

--- src/main/java/touro/peg/Move.java
+++ src/main/java/touro/peg/Move.java
@@ -1,13 +1,25 @@
 package touro.peg;
 
 public class Move {
-
-    boolean canMove() {
-
+    private int indexFrom;
+    private int indexRemove;
+    private int indexTo;
+    public Move(int indexFrom, int indexRemove, int indexTo){
+        this.indexFrom = indexFrom;
+        this.indexRemove = indexRemove;
+        this.indexTo = indexTo;
     }
 
-    void makeMove() {
+    public int getIndexFrom() {
+        return indexFrom;
+    }
 
+    public int getIndexRemove() {
+        return indexRemove;
+    }
+
+    public int getIndexTo() {
+        return indexTo;
     }
 
 }
