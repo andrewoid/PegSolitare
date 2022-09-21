8fdc146bdce66ff2d71251b4f5e1590ff879aa7e
chaya <chayaz.haor@gmail.com>
2022-09-21 19:21:52 -0400

    reformatted

--- src/main/java/touro/peg/TriangleBoard.java
+++ src/main/java/touro/peg/TriangleBoard.java
@@ -4,21 +4,22 @@ public class TriangleBoard {
 
     private boolean[] pegs = new boolean[15];
 
-    public TriangleBoard(int startingIndex){
+    public TriangleBoard(int startingIndex) {
         for (int i = 0; i < pegs.length; i++)
         {
             pegs[i] = i != startingIndex;
         }
     }
 
-    public boolean hasPeg(int index){
+    public boolean hasPeg(int index) {
         return pegs[index];
     }
 
-    private void printBoard(){
+    private void printBoard() {
         for (int i = 0; i < pegs.length; i++)
         {
-            for(int j = 0; j<i; j++){
+            for (int j = 0; j < i; j++)
+            {
                 System.out.print(pegs[i]);
             }
             System.out.println();
--- src/test/java/touro/peg/TriangleBoardTest.java
+++ src/test/java/touro/peg/TriangleBoardTest.java
@@ -2,18 +2,16 @@ package touro.peg;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class TriangleBoardTest {
     @Test
-    public void TriangleBoard() {
+    public void Constructor() {
         //Given
         int emptySpot = 0;
         //When
         TriangleBoard board = new TriangleBoard(emptySpot);
-        boolean[] expected =new boolean[]{false,true,true,true,true,true,true,true,true,true,true,true,true,true,true};
+        boolean[] expected = new boolean[]{false, true, true, true, true, true, true, true, true, true, true, true, true, true, true};
         //Then
 
         assertArrayEquals(board.getPegs(), expected);
