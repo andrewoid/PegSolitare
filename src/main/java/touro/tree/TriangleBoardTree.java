5821f99955a4d520dd3b9057141671109023236b
SaraYaffe <syaffe@student.touro.edu>
2022-10-19 15:32:38 -0400

    attempted to accommodate checkstyles

--- src/main/java/touro/tree/TriangleBoardTree.java
+++ src/main/java/touro/tree/TriangleBoardTree.java
@@ -9,9 +9,13 @@ import java.util.List;
 
 public class TriangleBoardTree {
     /*
-    * A tree is created with a given board in any state. A root node is created with the given board only.
-    * For every possible next move, a child of the current node is created containing the current state of the board,
-    * it's parent, and move. The child is added to the parent node's list of children.
+    * A tree is created with a given board in any state.
+    * A root node is created with the given board only.
+    * For every possible next move, a child of the current node is created containing
+    *   1. the current state of the board,
+    *   2. it's parent,
+    *   3. and move
+    * The child is added to the parent node's list of children.
     *
     * This is called recursively until no other moves can be made.
     * TODO: or until it is determined that no solution will be found for the current state of the board
@@ -36,11 +40,13 @@ public class TriangleBoardTree {
 
         @Override
         public String toString() {
-            return "\n" + triangleBoard +
-                    "\n" + ((move == null) ? "No move" : move) +
-                    "\nsize of children=" + children.size() + "\n";
+            return "\n"
+                    + triangleBoard
+                    + "\n" + ((move == null) ? "No move" : move)
+                    + "\nsize of children=" + children.size() + "\n";
         }
     }
+
     public TriangleBoardTree(TriangleBoard board) {
         TriangleTreeNode rootNode = new TriangleTreeNode(board, null, null);
         this.leaves = new ArrayList<>();
@@ -51,6 +57,7 @@ public class TriangleBoardTree {
     public List<TriangleTreeNode> getLeaves() {
         return leaves;
     }
+
     private void createTreeAndStoreLeaves(TriangleTreeNode node, TriangleBoard board) {
         for (Move legalMove : board.getPlayMove().getLegalMoves()) {
             if (board.getPlayMove().isValidMove(legalMove)) {
@@ -61,18 +68,19 @@ public class TriangleBoardTree {
                 createTreeAndStoreLeaves(newTriangleTreeNode, copyBoard);
             }
         }
-        if(node.children.size() == 0)
+        if (node.children.size() == 0) {
             leaves.add(node);
+        }
     }
 
     /*  Given a TriangleTreeNode, this method returns a List of TriangleTreeNodes that resulted in the given node */
-    public List<TriangleTreeNode> getSequenceToNode(TriangleTreeNode treeNode){
+    public List<TriangleTreeNode> getSequenceToNode(TriangleTreeNode treeNode) {
         TriangleTreeNode node = treeNode;
         List<TriangleTreeNode> listOfMovesToNode = new ArrayList<>();
         listOfMovesToNode.add(node);
-        while(node.parent != null){
+        while (node.parent != null) {
             node = node.parent;
-            listOfMovesToNode.add(0,node);
+            listOfMovesToNode.add(0, node);
         }
         return listOfMovesToNode;
 
