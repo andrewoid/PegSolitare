1579486690bb4589ce8a70116493fdfaed5cb022
chaya <chayaz.haor@gmail.com>
2022-10-19 16:01:32 -0400

    tested false winning board

--- src/test/java/touro/peg/TriangleBoardTest.java
+++ src/test/java/touro/peg/TriangleBoardTest.java
@@ -33,29 +33,56 @@ class TriangleBoardTest {
     @Test
     public void isWin(){
         //Given
-        //When
         TriangleBoard board = new TriangleBoard(0);
         for (int i = 2; i < 15; i++)
         {
             board.setPeg(i, false);
         }
+        //When
         //Then
-
         assertTrue(board.isWin());
     }
 
     @Test
     public void isBestWin(){
         //Given
-        //When
         TriangleBoard board = new TriangleBoard(0);
         board.setPeg(0, true);
         for (int i = 1; i < 15; i++)
         {
             board.setPeg(i, false);
         }
+        //When
         //Then
         assertTrue(board.isBestWin());
     }
+
+    @Test
+    public void isFalseWin(){
+        //Given
+        TriangleBoard board = new TriangleBoard(0);
+        for (int i = 2; i < 15; i++)
+        {
+            board.setPeg(i, true);
+        }
+        //When
+        //Then
+        assertFalse(board.isWin());
+    }
+
+    @Test
+    public void isFalseBestWin(){
+        //Given
+        TriangleBoard board = new TriangleBoard(1);
+        board.setPeg(0, true);
+        for (int i = 1; i < 15; i++)
+        {
+            board.setPeg(i, false);
+        }
+        //When
+        //Then
+        assertFalse(board.isBestWin());
+    }
+
 
 }
